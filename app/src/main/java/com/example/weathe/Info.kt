9479f7a9c734357719bfb3d0462327e2b5724a2f
ZineEddin ChikhTouami <zinouchikhtouami2000@gmail.com>
2025-05-29 19:06:30 +0100

    start fetching data

--- app/src/main/java/com/example/weathe/Info.kt
+++ app/src/main/java/com/example/weathe/Info.kt
@@ -1,20 +1,35 @@
 package com.example.weathe
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.weathe.databinding.ActivityInfoBinding
+import com.squareup.picasso.Picasso
 
 class Info : AppCompatActivity() {
+    lateinit var binding : ActivityInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val name = intent.getStringExtra("name")
+        val how = intent.getStringExtra("how")
+        val tempC = intent.getStringExtra("temp_c")
+        val country = intent.getStringExtra("country")
+        val localtime = intent.getStringExtra("localtime")
+        val icon = intent.getStringExtra("icon")
+
+
+        binding.name.text = name
+        binding.how.text = how
+        binding.tempC.text = tempC
+        binding.country.text = country
+        binding.localtime.text = localtime
+    Picasso.get().load(icon.toString()).into(binding.img)
+
     }
 }
--- app/src/main/java/com/example/weathe/MainActivity.kt
+++ app/src/main/java/com/example/weathe/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.weathe
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.weathe.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
@@ -17,7 +23,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.searchBtn.setOnClickListener {
-            startActivity(Intent(this,Info::class.java))
+            if (binding.searchBar.text.toString().trim() != "") {
+                fetchData("https://api.weatherapi.com/v1/current.json?key=47e9cf9b7636453890f173006240601&q=${binding.searchBar.text.toString()}&aqi=no")
+            }
         }
+    }
+    private fun fetchData(urlString: String) {
+        Thread {
+            try {
+                val url = URL(urlString)
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+                connection.connect()
+
+                val responseText = connection.inputStream.bufferedReader().readText()
+                val jsonObject = JSONObject(responseText)
+
+                val name = jsonObject.getJSONObject("location").getString("name")
+                val country = jsonObject.getJSONObject("location").getString("country")
+                val localtime = jsonObject.getJSONObject("location").getString("localtime")
+                val temp_c = jsonObject.getJSONObject("current").getString("temp_c")
+                val how = jsonObject.getJSONObject("current").getJSONObject("condition").getString("text")
+                val icon = jsonObject.getJSONObject("current").getJSONObject("condition").getString("icon")
+
+                runOnUiThread {
+                    val i = Intent(this,Info::class.java)
+                    i.putExtra("name",name)
+                    i.putExtra("country",country)
+                    i.putExtra("localtime",localtime)
+                    i.putExtra("temp_c",temp_c)
+                    i.putExtra("how",how)
+                    i.putExtra("icon",icon)
+                    startActivity(i)
+                }
+
+            }catch(e:Exception) {
+                runOnUiThread {
+                    Toast.makeText(this,"error",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.start()
     }
 }
